966e68a74bd82e49544512c5d598c7a8ad14257d
bella-cd <57729914+bella-cd@users.noreply.github.com>
2023-04-15 18:26:37 +0100

    Give feedback according to the user guess

--- app/src/main/java/pt/ipg/guess_number/MainActivity.kt
+++ app/src/main/java/pt/ipg/guess_number/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
@@ -27,7 +28,20 @@ class MainActivity : AppCompatActivity() {
 
         if (guess == null){
             editTextNumber.error = "Invalid Number"
-            return 
+            return
+        }
+
+        if (guess !in 1..10 ){
+            editTextNumber.error = " The number must be between 1 and 10 "
+            return
+
+        }
+        if (guess == numberToGuess){
+
+            Toast.makeText(this, "Congratulation you guessed the number", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            Toast.makeText(this, "Wrong guess", Toast.LENGTH_SHORT).show()
         }
     }
 
